b221a80b29a581a1b26107c6e45ded798a3e5852
Karolina Jaskowska <Karolina.Jaskowska@microsoft.wsei.edu.pl>
2022-04-20 18:25:47 +0200

    copierclass2

--- Copier/Zadanie1/Copier.cs
+++ Copier/Zadanie1/Copier.cs
@@ -2,11 +2,53 @@
 
 namespace ver1
 {
-    public class Copier
+    public class Copier :BaseDevice, IPrinter, IScanner
     {
         public int PrintCounter { get; set; } = 0;
         public int ScanCounter { get; set; } = 0;
         public int Counter { get; set; } = 0;
+
+        public void PowerOn()
+        {
+            base.PowerOn();
+            Counter++;
+        }
+
+        public void Print(in IDocument document)
+        {
+            DateTime localDate = DateTime.Now;
+            if (state == IDevice.State.on)
+            {
+                Console.WriteLine(localDate+" Print: "+document.GetFileName());
+                PrintCounter++;
+            }
+        }
+        public void Scan(out IDocument document, IDocument.FormatType formatType = IDocument.FormatType.PDF)
+        {
+            DateTime localDate = DateTime.Now;
+            string type = "";
+            string nameFile = string.Format(type + "Scan" + ScanCounter + 1 + "." + formatType.ToString());
+            switch (formatType)
+            {
+                case IDocument.FormatType.PDF:
+                    type = "PDF";
+                    document = new PDFDocument(nameFile);
+                    break;
+                case IDocument.FormatType.JPG:
+                    type = "JPG";
+                    document = new ImageDocument(nameFile);
+                    break;
+                case IDocument.FormatType.TXT:
+                    type = "TXT";
+                    document = new TextDocument(nameFile);
+                    break;
+            }
+            if (state == IDevice.State.on)
+            {
+                Console.WriteLine(localDate + ", Scan: " + document.GetFileName());
+                ScanCounter++;
+            }
+        }
     }
 }
 
