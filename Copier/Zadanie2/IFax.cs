80cea044d275212a13fb251e07f3f0cab54bb541
Karolina Jaskowska <Karolina.Jaskowska@microsoft.wsei.edu.pl>
2022-04-21 18:22:05 +0200

    zadanie2

---
+++ Copier/Zadanie2/IFax.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zadanie1;
+namespace Zadanie2
+{
+    public interface IFax : IDevice
+    {
+        void Fax(in IDocument document, string numberOfFax);
+    }
+}
---
+++ Copier/Zadanie2/MultifunctionalDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zadanie1;
+namespace Zadanie2
+{
+    class MultifunctionalDevice : Copier, IFax
+    {
+        public int FaxCounter { get; set; } = 0;
+    }
+}
